200360179215284746adfda94e2c131abf708732
Zheren Xu <xuzheren1998@gmail.com>
2021-11-18 21:56:45 -0800

    downloader.py done

--- downloader.py
+++ downloader.py
@@ -1,13 +1,13 @@
 # author: Melisa Maidana, Steven Lio, Zheren Xu
 # date: 2021-11-18
 
-'''This script downloads data set from given URL and writes it to local.
+'''This script downloads .tsv.gz format data set from given URL and writes it to local.
 This script takes a URL and a local file path.
 
 Usage: downloader.py <url> <path>
 
 Options:
-<url>              Data set URL (Required)
+<url>              data set URL (Required)
 <path>             Write path (Required)
 '''
 
@@ -16,4 +16,12 @@ import numpy as np
 from docopt import docopt
 
 opt = docopt(__doc__)
-print(opt["<url>"])
+
+def main(url, path):
+    file_name = url.split('/')[-1].split('.gz')[0]
+    df = pd.read_csv(url, sep='\t')
+    df.to_csv(path+'/'+file_name, sep = '\t', index=False)
+    print("Download Is Done")
+  
+if __name__ == "__main__":
+    main(opt["<url>"], opt["<path>"])
